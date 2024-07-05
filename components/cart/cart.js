7be4802b18c43c2cb0ea33e9fa0ff788c1bed2f4
TrungLe2003 <trungle.kqt@gmail.com>
2024-07-05 22:40:09 +0700

    sualan3

--- components/cart/cart.js
+++ components/cart/cart.js
@@ -1,3 +1,6 @@
+const totalPriceElement = document.getElementById("total-price");
+let totalPrice = 0;
+
 //Lấy thông tin sản phẩm
 const data = JSON.parse(localStorage.getItem("product-choice") || "[]");
 console.log(data);
@@ -7,9 +10,11 @@ if (data.length === 0) {
   product.innerHTML = `<p>Giỏ hàng trống</p>`;
 }
 for (let i = 0; i < data.length; i++) {
+  const itemTotalPrice =
+    parseFloat(data[i].priceProduct) * parseInt(data[i].count); //chuyển đổi dạng string sang só thực và số nguyên
+  totalPrice += itemTotalPrice;
   product.innerHTML += `
-                <div class="frame1-item">
-                    <div class="frame1-col">
+                     <div class="frame1-col">
                         <div class="frame1-cate-item">
                                 <div class="frame1-cate-img">
                                     <img src="../${data[i].imgProduct}" alt="" class="frame1-img">
@@ -20,13 +25,12 @@ for (let i = 0; i < data.length; i++) {
                                     <div class="frame1-product-card-row">
                                         <p class="frame1-product-card-price">${data[i].priceProduct}đ x ${data[i].count}</p>                                                                                       
                                     </div>
-                                </div>
-                                               
-                    </div>
-                    <button>+</button>
-                    <button>-</button>
-                        
-                </div> `;
+                                    </div>
+                                    <div class = "incr-decr-btn">
+                                      <button class="increase-btn" data-id="${data[i].id}">+</button>
+                                      <button class="decrease-btn" data-id="${data[i].id}">-</button>
+                                    </div>                                               
+                    </div>`;
 }
 
 //Phần giỏ hàng
@@ -76,4 +80,30 @@ function getTotal() {
 getTotal();
 
 //Tổng số tiền
-const totalPrice = document.getElementById("total-price");
+totalPriceElement.innerHTML = `${totalPrice.toLocaleString()},000đ`; //định dạng số tiền thành chuỗi có dấu phân cách hàng nghìn.
+
+// Nút tăng giảm số lượng sản phẩm
+document.querySelectorAll(".increase-btn").forEach((button) => {
+  //Lấy tất cả button có class increase-button //gắn sự kiện cho mỗi button
+  button.addEventListener("click", (e) => {
+    const id = e.target.getAttribute("data-id"); // lấy giá trị của thuộc tính data-id từ phần tử e -e.target: trả về phần tử kích hoạt sự kiện click button-, biến id lưu trữ giá trị thuộc tính data-id
+    const product = data.find((item) => item.id == id);
+    product.count++;
+    localStorage.setItem("product-choice", JSON.stringify(data));
+    window.location.reload(); //Load lại trang để cập nhật lại product.count cho nhanh :))
+  });
+});
+
+document.querySelectorAll(".decrease-btn").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const id = e.target.getAttribute("data-id");
+    const product = data.find((item) => item.id == id);
+    if (product.count > 1) {
+      product.count--;
+    } else {
+      data.splice(data.indexOf(product), 1); //ở vị trí product có id đó, xóa đi 1 phần tử
+    }
+    localStorage.setItem("product-choice", JSON.stringify(data));
+    window.location.reload();
+  });
+});
