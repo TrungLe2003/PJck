5919708ed2970c21c56e1eee8518b56959011691
TrungLe2003 <trungle.kqt@gmail.com>
2024-07-06 19:04:04 +0700

    sua lan 4

--- components/cart/cart.js
+++ components/cart/cart.js
@@ -1,3 +1,66 @@
+// Chuyển đổi đăng nhập - đăng ký > giỏ hàng - user
+let currentSignIn = localStorage.getItem('currentSignIn')
+console.log(currentSignIn)
+const act = document.getElementById('act')
+if (currentSignIn) {
+    currentSignIn = JSON.parse(currentSignIn)
+    act.innerHTML = `
+        <div class="cart">
+            <a href="./components/cart/cart.html">
+                <img src="../../public/icon/cart-shopping-svgrepo-com.svg" alt="" class="act-icon act-icon-cart">
+                <div class="total" id="total">0</div>
+            </a>
+        </div>
+
+        <div class="account">
+            <img src="../../public/icon/user-svgrepo-com.svg" alt="" class="act-icon">
+            <div class="username">${currentSignIn.username}</div>
+        </div>
+
+        <button class="act-button sign-out" id="sign-out">Đăng Xuất</button>
+    `
+    // Đăng xuất
+    const signOut = document.getElementById('sign-out');
+    signOut.addEventListener('click', () => {
+        localStorage.removeItem('currentSignIn');
+        localStorage.removeItem('product-choice');
+        window.location.href = "../../../pages/homepage/homepage.html";
+    });
+} else {
+    act.innerHTML = `
+        <div class="cart">
+            <a href="./components/cart/cart.html">
+                <img src="../../public/icon/cart-shopping-svgrepo-com.svg" alt="" class="act-icon act-icon-cart">
+                <div class="total" id="total">0</div>
+            </a>
+        </div>
+
+        <a href="../../pages/sign-in/sign-in.html">
+            <button class="act-button sign-in">Đăng Nhập</button>
+        </a>
+        
+        <a href="../../pages/sign-up/sign-up.html">
+            <button class="act-button sign-up">Đăng Ký</button>
+        </a>
+    `
+}
+
+
+// Hiển thị số lượng sản phẩm đã thêm vào giỏ hàng
+function getTotal() {
+    const total = document.getElementById('total')
+    const products = JSON.parse(localStorage.getItem('product-choice') || '[]')
+    const productCount = products.reduce((number, item) => {
+        number += item.count
+        return number
+    }, 0)
+    console.log(products);
+    total.innerHTML = `${productCount}`
+}
+getTotal();
+
+
+
 const totalPriceElement = document.getElementById("total-price");
 let totalPrice = 0;
 
@@ -14,70 +77,22 @@ for (let i = 0; i < data.length; i++) {
     parseFloat(data[i].priceProduct) * parseInt(data[i].count); //chuyển đổi dạng string sang só thực và số nguyên
   totalPrice += itemTotalPrice;
   product.innerHTML += `
-                     <div class="frame1-col">
-                        <div class="frame1-cate-item">
-                                <div class="frame1-cate-img">
-                                    <img src="../${data[i].imgProduct}" alt="" class="frame1-img">
-                                </div>
+        <div class="frame1-row">
+            <img src="../${data[i].imgProduct}" alt="" class="frame1-product-img">
 
-                                <div class="frame1-cate-item-info">
-                                    <h3 class="frame1-cate-item-title">${data[i].nameProduct}</h3>
-                                    <div class="frame1-product-card-row">
-                                        <p class="frame1-product-card-price">${data[i].priceProduct}đ x ${data[i].count}</p>                                                                                       
-                                    </div>
-                                    </div>
-                                    <div class = "incr-decr-btn">
-                                      <button class="increase-btn" data-id="${data[i].id}">+</button>
-                                      <button class="decrease-btn" data-id="${data[i].id}">-</button>
-                                    </div>                                               
-                    </div>`;
+            <div class="frame1-product-infor">
+                <h1 class="frame1-product-name">${data[i].nameProduct}</h1>
+                <p class="frame1-product-price">${data[i].priceProduct}đ x ${data[i].count}</p>                                                                                       
+                <div class = "incr-decr-btn">
+                    <button class="increase-btn" data-id="${data[i].id}">+</button>
+                    <button class="decrease-btn" data-id="${data[i].id}">-</button>
+                </div>
+            </div>                                           
+        </div>
+    `
 }
 
-//Phần giỏ hàng
-let currentSignIn = localStorage.getItem("currentSignIn");
-console.log(currentSignIn);
-const act = document.getElementById("act");
-if (currentSignIn) {
-  currentSignIn = JSON.parse(currentSignIn);
-  act.innerHTML = `
-        <div class="cart">
-            <a href="../../components/cart/cart.html">
-                <img src="../../public/icon/cart-shopping-svgrepo-com.svg" alt="" class="act-icon act-icon-cart">
-                <div class="total" id="total-product">0</div>
-            </a>
-        </div>
 
-        <div class="account">
-            <img src="../../public/icon/user-svgrepo-com.svg" alt="" class="act-icon">
-            <div>${currentSignIn.username}</div>
-        </div>
-    `;
-} else {
-  act.innerHTML = `
-        <div class="cart">
-            <img src="../../public/icon/cart-shopping-svgrepo-com.svg" alt="" class="act-icon act-icon-cart">
-            <div class="total" id="total">0</div>
-        </div>
-
-        <a href="../sign-in/sign-in.html">
-            <button class="act-button sign-in">Sign In</button>
-        </a>
-        
-        <a href="../sign-up/sign-up.html">
-            <button class="act-button sign-up">Sign Up</button>
-        </a>
-    `;
-}
-
-function getTotal() {
-  const totalProduct = document.getElementById("total-product");
-  const productCount = data.reduce((number, item) => {
-    number += item.count;
-    return number;
-  }, 0);
-  totalProduct.innerHTML = `${productCount}`;
-}
-getTotal();
 
 //Tổng số tiền
 totalPriceElement.innerHTML = `${totalPrice.toLocaleString()},000đ`; //định dạng số tiền thành chuỗi có dấu phân cách hàng nghìn.
--- components/products/products.js
+++ components/products/products.js
@@ -1,10 +1,10 @@
 // Hiển thị giỏ hàng - user
-let currentSignIn = localStorage.getItem("currentSignIn");
-console.log(currentSignIn);
-const act = document.getElementById("act");
+let currentSignIn = localStorage.getItem('currentSignIn')
+console.log(currentSignIn)
+const act = document.getElementById('act')
 if (currentSignIn) {
-  currentSignIn = JSON.parse(currentSignIn);
-  act.innerHTML = `
+    currentSignIn = JSON.parse(currentSignIn)
+    act.innerHTML = `
         <div class="cart">
             <a href="../cart/cart.html"">
                 <img src="../../../public/icon/cart-shopping-svgrepo-com.svg" alt="" class="act-icon act-icon-cart">
@@ -14,31 +14,45 @@ if (currentSignIn) {
 
         <div class="account">
             <img src="../../../public/icon/user-svgrepo-com.svg" alt="" class="act-icon">
-            <div>${currentSignIn.username}</div>
+            <div class="username">${currentSignIn.username}</div>
         </div>
-    `;
+
+        <button class="act-button sign-out" id="sign-out">Đăng Xuất</button>
+    `
+    // Đăng xuất
+    const signOut = document.getElementById('sign-out');
+    signOut.addEventListener('click', () => {
+        localStorage.removeItem('currentSignIn');
+        localStorage.removeItem('product-choice');
+        window.location.href = "../../../pages/homepage/homepage.html";
+    });
+} else {
+    act.innerHTML = `
+        <div class="cart">
+            <a href="../cart/cart.html"">
+                <img src="../../../public/icon/cart-shopping-svgrepo-com.svg" alt="" class="act-icon act-icon-cart">
+                <div class="total" id="total">0</div>
+            </a>
+        </div>
+
+        <a href="../../pages/sign-in/sign-in.html">
+            <button class="act-button sign-in">Đăng Nhập</button>
+        </a>
+        
+        <a href="../../pages/sign-up/sign-up.html">
+            <button class="act-button sign-up">Đăng Ký</button>
+        </a>
+    `
 }
 
-function getTotal() {
-  const total = document.getElementById("total");
-  const products = JSON.parse(localStorage.getItem("product-choice") || "[]");
-  const productCount = products.reduce((number, item) => {
-    number += item.count;
-    return number;
-  }, 0);
-  console.log(products);
-  total.innerHTML = `${productCount}`;
-}
 
 // Hiển thị thông tin sản phẩm
 const product = document.getElementById("product");
 const productNumber = +localStorage.getItem("productNumber");
-const productsInfor = JSON.parse(
-  localStorage.getItem("products-infor") || "[]"
-);
+const productsInfor = JSON.parse(localStorage.getItem("products-infor") || "[]");
 const findProduct = productsInfor.find((item) => +item.id === productNumber);
 if (findProduct) {
-  product.innerHTML = `
+    product.innerHTML = `
         <div class="frame1">
             <p>Trang chủ</p>
             <img src="../../public/icon/right-arrow-svgrepo-com.svg" alt="" class="frame1-img">
@@ -67,7 +81,7 @@ if (findProduct) {
                             <img src="../../../public/icon/star-svgrepo-com.svg" alt="" class="frame2-card-star">
                         </div>
 
-                        <div class="add-to-cart" id= "add-to-cart">Thêm vào giỏ hàng</div>
+                        <div class="add-to-cart" id="add-to-cart">Thêm vào giỏ hàng</div>
                     </div>
                 </div>
             </div>
@@ -97,35 +111,44 @@ if (findProduct) {
                 </table>
             </div>
         </div>
-    `;
+    `        
 }
 
-// Thêm sản phẩm vào giỏ hàng
-const addToCartButton = document.getElementById("add-to-cart");
-addToCartButton.addEventListener("click", () => {
-  const currentSignIn = localStorage.getItem("currentSignIn");
-  if (!currentSignIn) {
-    // console.log('Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng');
-    alert("Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng");
-    window.location.href = "../../pages/sign-in/sign-in.html";
-    return;
-  }
-  const data = JSON.parse(localStorage.getItem("product-choice") || "[]");
-  const findProductInCart = data.findIndex(
-    (item) => item.id === findProduct.id
-  );
-  //   console.log(findProductInCart);
-  if (findProductInCart === -1) {
-    data.push({
-      ...findProduct,
-      count: 1,
-    });
-  } else {
-    data[findProductInCart].count++;
-  }
-  //   console.log(data);
-  localStorage.setItem("product-choice", JSON.stringify(data));
-  getTotal();
-});
 
-getTotal();
+// Hiển thị số lượng sản phẩm đã thêm vào giỏ hàng
+function getTotal() {
+    const total = document.getElementById('total')
+    const products = JSON.parse(localStorage.getItem('product-choice') || '[]')
+    const productCount = products.reduce((number, item) => {
+        number += item.count
+        return number
+    }, 0)
+    console.log(products);
+    total.innerHTML = `${productCount}`
+}
+// Thêm sản phẩm vào giỏ hàng
+const addToCart = document.getElementById('add-to-cart')
+addToCart.addEventListener('click', () => {
+    const currentSignIn = localStorage.getItem('currentSignIn')
+    if (!currentSignIn) {
+        // console.log('Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng');
+        alert('Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng')
+        window.location.href = "../../pages/sign-in/sign-in.html"
+        return
+    }
+    const data = JSON.parse(localStorage.getItem('product-choice') || '[]')
+    const findProductInCart = data.findIndex((item) => item.id === findProduct.id)
+    // console.log(findProduct);
+    if (findProductInCart === -1) {
+        data.push({
+            ...findProduct,
+            count: 1,
+        })
+    } else {
+        data[findProductInCart].count++
+    }
+    // console.log(data);
+    localStorage.setItem('product-choice', JSON.stringify(data))
+    getTotal()
+})
+getTotal()
